6e833c1f22f755bbe3a59eeae3bd563ab5404807
second1996 <vital10012@gmail.com>
2020-08-15 19:53:22 +0300

    Update

--- app/js/app.js
+++ app/js/app.js
@@ -249,4 +249,14 @@ jQuery(document).ready(function($) {
 		})
 	}
 
+	$('.profile-orders-table').footable({
+		// "toggleColumn": "last",
+		"breakpoints": {
+			"xxs": 320,
+			"xs": 576,
+			"sm": 768,
+			"md": 992,
+			"lg": 1278
+		}
+	});
 })
